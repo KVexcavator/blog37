42f1602febfc4fc3b2cea6638adcd9c550254151
KVexcavator <str_token@mail.ru>
2018-11-03 18:39:58 +0600

    add aditional comments to articles

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :article do
    title "Article title"
    text "Article text"
+
+   #создаём фабрику с названием article_with_comments
+   # для создания статьи с несколькими комментариями
+   factory :article_with_comments do
+    # после сщздания article
+    after :create do |article, evaluator|
+      # создаём список из 3-Х коментариев
+      create_list :comment,3, article: article
+    end
+   end
   end
 end
