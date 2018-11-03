3280850600d8164c6ad33166b8d4ea20dac57998
KVexcavator <str_token@mail.ru>
2018-11-03 14:42:44 +0600

    ervemp  mapping spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  it { should have_many :comments }
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :text  }
+
+  describe "associations" do
+    it { should have_many :comments }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text  }
+  end
+
 end
