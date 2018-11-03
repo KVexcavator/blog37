9add0647286b4fbda3738369a7955dad6f354443
KVexcavator <str_token@mail.ru>
2018-11-03 20:04:24 +0600

    add test length new article rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,9 +6,18 @@ RSpec.describe Article, type: :model do
     it { should have_many :comments }
   end
 
-  describe "validations" do
-    it { should validate_presence_of :title }
-    it { should validate_presence_of :text  }
+  describe "validations the length" do
+    it do
+    should validate_length_of(:title ).
+      is_at_least(2).is_at_most(140)
+    end
+
+    it do
+    should validate_length_of(:text).
+      is_at_least(10).is_at_most(4000)
+    end
+    #it { should validate_presence_of :title }
+    #it { should validate_presence_of :text  }
   end
 
   describe "#subject" do
