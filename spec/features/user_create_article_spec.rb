1ab53654e1c2a44f375aa2c6f885f6ec12ae15f9
KVexcavator <str_token@mail.ru>
2018-11-04 18:59:00 +0600

    END 47: add test allows user to visit new article page

---
+++ spec/features/user_create_article_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+
+feature "Article creation" do
+  before(:all) do
+    sing_up
+  end
+  # тест входа на страницу создания статьи
+  scenario "allows user to visit new article page" do    
+    visit new_article_path
+    expect(page).to have_content "New article"
+  end
+
+end
