6d9f23d405a8a5c258c2e517f94de5c3c6cb4321
KVexcavator <str_token@mail.ru>
2018-11-05 19:10:40 +0600

    update test visit article to create article

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -5,10 +5,16 @@ feature "Article creation" do
   before(:all) do
     sing_up
   end
-  # тест входа на страницу создания статьи
-  scenario "allows user to visit new article page" do    
+  # тест входа на страницу создания статьи и создание статьи
+  scenario "allows user to visit new article page and create article" do    
     visit new_article_path
-    expect(page).to have_content I18n.t('articles.article_new')
+
+    fill_in :article_title, :with => 'barmaglot'
+    fill_in :article_text, :with => "texttexttexttext"
+
+    click_button  I18n.t('articles.article_send_save_article')
+
+    expect(page).to have_content 'barmaglot'
   end
 
 end
